15b7492a2fc39658ad1a38d5d27f433006446d27
Atila Media Pratama <50263032+atilamedia@users.noreply.github.com>
2019-06-07 11:17:56 +0800

    Update start.sh

--- install/start.sh
+++ install/start.sh
@@ -12,6 +12,6 @@ if [ -f "/usr/bin/apt-get" ];then
 		exit;
 	fi
 else 
-	wget -O install.sh https://raw.githubusercontent.com/atilamedia/bt-panel/master/install/install-centos.sh && sudo bash install.sh
+	wget -O install.sh https://raw.githubusercontent.com/atilamedia/bt-panel/master/install/install-centos.sh && sh install.sh
 	exit;
 fi
