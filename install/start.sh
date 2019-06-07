32ab9fb4c972b6b34d1f51992488db8b2b6587f5
Atila Media Pratama <50263032+atilamedia@users.noreply.github.com>
2019-06-07 11:01:19 +0800

    Update start.sh

--- install/start.sh
+++ install/start.sh
@@ -5,13 +5,13 @@ export PATH
 if [ -f "/usr/bin/apt-get" ];then
 	isDebian=`cat /etc/issue|grep Debian`
 	if [ "$isDebian" != "" ];then
-		apt install wget -y && wget -O install.sh https://raw.githubusercontent.com/atilamedia/bt-panel/master/install/install-ubuntu.sh && bash install.sh
+		wget -O install.sh https://raw.githubusercontent.com/atilamedia/bt-panel/master/install/install-ubuntu.sh && bash install.sh
 		exit;
 	else
-		apt install wget -y && wget -O install.sh https://raw.githubusercontent.com/atilamedia/bt-panel/master/install/install-ubuntu.sh && sudo bash install.sh
+		wget -O install.sh https://raw.githubusercontent.com/atilamedia/bt-panel/master/install/install-ubuntu.sh && sudo bash install.sh
 		exit;
 	fi
 else 
-	yum install wget -y && wget -O install.sh https://raw.githubusercontent.com/atilamedia/bt-panel/master/install/install-centos.sh && sudo bash install.sh
+	wget -O install.sh https://raw.githubusercontent.com/atilamedia/bt-panel/master/install/install-centos.sh && sudo bash install.sh
 	exit;
 fi
