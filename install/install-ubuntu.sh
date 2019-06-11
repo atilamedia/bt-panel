aa129090ab905bd46cde55b48202a22b7428427c
Atila Media Pratama <50263032+atilamedia@users.noreply.github.com>
2019-06-11 18:41:39 +0800

    Update install-ubuntu.sh

--- install/install-ubuntu.sh
+++ install/install-ubuntu.sh
@@ -386,9 +386,9 @@ else
 	fi
 fi
 
-#wget -O /www/server/panel/install/lib.sh https://raw.githubusercontent.com/atilamedia/bt-panel/master/install/lib.sh
-#sh /www/server/panel/install/lib.sh
-#sleep 3
+wget -O /www/server/panel/install/lib.sh https://raw.githubusercontent.com/atilamedia/bt-panel/master/install/lib.sh
+sh /www/server/panel/install/lib.sh
+sleep 3
 
 #wget -O /www/server/panel/install/nginx.sh https://raw.githubusercontent.com/atilamedia/bt-panel/master/install/nginx.sh
 #sh /www/server/panel/install/nginx.sh install 1.12
